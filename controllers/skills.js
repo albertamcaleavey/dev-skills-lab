f9761d08143abe59be3b9fa17a1bb48f0c9a423d
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-02-24 17:39:22 -0500

    add route and controller function for create new skill

--- controllers/skills.js
+++ controllers/skills.js
@@ -22,8 +22,18 @@ function newSkill(req, res) {
   res.render('skills/new')
 }
 
+function create(req, res) {
+  console.log(req.body)
+  // creates a new skill in the "database", passing in the new skill (text entered) and redirect function
+  skillDb.create(req.body, function(error, todo) {
+    // redirects to list of skills page to see newly create skill
+    res.redirect('/skills')
+  })
+}
+
 export {
   index,
   show,
-  newSkill as new
+  newSkill as new,
+  create
 }
--- routes/skills.js
+++ routes/skills.js
@@ -5,10 +5,12 @@ const router = Router()
 /* GET users listing. */
 // route to controller function that renders a view of all skills
 router.get('/', skillsCtrl.index)
-// route to controller function that renders a view of the newly created skill
+// route to controller function that renders a view of the enter new skill page
 router.get('/new', skillsCtrl.new)
 // route to controller function that renders a view of each skill
 router.get('/:id', skillsCtrl.show)
+// route to function that creates a new skill
+router.post('/', skillsCtrl.create)
 
 export {
   router
